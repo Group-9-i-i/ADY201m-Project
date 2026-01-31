fd8d254b2a673cb6323f8ca1ee7cc656cdbd8778
long2110d <90055332+long2110d@users.noreply.github.com>
2026-01-31 10:51:06 +0700

    Update testdata.py

--- New folder/testdata.py
+++ New folder/testdata.py
@@ -1,2 +1,6 @@
 import pandas as pd
-df = pd.read_csv('')
+df = pd.read_csv('data_season_with_ndvi_fixed.csv')
+mb = df.groupby('Location')['yeilds'].mean()
+isnull= df.isnull().sum()
+print(df.describe())
+print(df.duplicated().sum())
