89da5e91a7491b674ffb5fc57affe2eb8f449d1a
long2110d <90055332+long2110d@users.noreply.github.com>
2026-02-02 19:54:07 +0700

    Update datacleaning_test1.py

--- New folder/datacleaning_test1.py
+++ New folder/datacleaning_test1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # 1. Tải bộ dữ liệu chính
-df = pd.read_csv('data_enriched_npk.csv')
+df = pd.read_csv('data_season.csv')
 
 # Kiểm tra nhanh dữ liệu ban đầu
 print("Số dòng/cột ban đầu:", df.shape)
