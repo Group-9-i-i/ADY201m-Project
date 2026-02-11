c2049a68c6e4b8795ab58c77c6c781fc4b398e95
long2110d <90055332+long2110d@users.noreply.github.com>
2026-02-11 08:33:16 +0700

    đổi tên file

--- baladesh data/testclean.py
+++ baladesh data/testclean.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv("ket_qua_ghep.csv")
+df = pd.read_csv("ket_qua_ghep_updated_ph.csv")
 print(df.head())
 print(df.columns)
 print(df.info())
 print(df.describe())
 print(df.isnull().sum())
 print(df["District"].unique())
+print(df.corr())
