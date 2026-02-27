40a57d4c9a838d5fc0337d4c10e4db9e98adcdc7
long2110d <90055332+long2110d@users.noreply.github.com>
2026-02-27 20:42:37 +0700

    Create sql_convert.py

---
+++ SQL/sql_convert.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# đọc csv
+df = pd.read_csv("ket_qua_ghep.csv", encoding="utf-8-sig")
+
+print(df.head())
+print(df.info())
+
+engine = create_engine(
+    "mssql+pyodbc://@LAPTOP-NVSURACC\\SQLEXPRESS/testdb"
+    "?driver=ODBC+Driver+17+for+SQL+Server"
+    "&trusted_connection=yes"
+)
+
+
+
+
+df.to_sql(
+    "data_sau_clean",
+    engine,
+    if_exists="replace",
+    index=False,
+    chunksize=1000   # import nhanh hơn
+)
+
+print("Import xong")
